c1721349154cacff3723b602e84dd1dc70c60ff4
baydogan <aydoganbugra93@gmail.com>
2022-03-13 02:45:39 +0300

    search bar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,19 +1,34 @@
-import './index.css'
-import { GrSearch } from 'react-icons/gr'
-import {GrFormClose} from 'react-icons/gr'
+import "./index.css";
+import { GrSearch } from "react-icons/gr";
+import { GrFormClose } from "react-icons/gr";
+import { IoMdDownload } from "react-icons/io";
+import { IoMdLink } from "react-icons/io";
 
 const Searchbar = () => {
-    return (
-        <div className='search-bar'>
-            <div className='search-bar-group'>
-                <div className='search-input'>
-                    <GrSearch className='search-icon' />
-                    <input type="search" placeholder='Search Brands' />
-                    <GrFormClose className='close-icon' />
-                </div>
-            </div>
+  return (
+    <div className="search-bar">
+      <div className="search-bar-group">
+        <div className="search-input">
+          <GrSearch className="search-icon" />
+          <input type="search" placeholder="Search Brands" />
+          <GrFormClose className="close-icon" />
         </div>
-    );
-}
+      </div>
+      <div className="search-bar-group">
+        <div className="toolbar-collection">
+          <IoMdDownload />
+          <IoMdLink />
+          <GrFormClose />
+          <span>0 brands collected</span>
+        </div>
+        <div className="toolbar-download">
+          <IoMdDownload />
+          <span>All Brands</span>
+          <select name="" id=""></select>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Searchbar;
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,5 +1,5 @@
 import Logo from "../../assets/img/brandcolors.jpeg";
-import './index.css'
+import "./index.css";
 
 const Sidebar = () => {
   return (
@@ -11,26 +11,28 @@ const Sidebar = () => {
             Brand<strong>Colors</strong>
           </a>
         </h1>
-        <h2>The biggest collection of official brand color codes around. Curated by @brandcolors and friends.</h2>
-        <nav>
-          <ul>
-            <li>
-              <a href="">Suggest a Brand</a>
-            </li>
-            <li>
-              <a href="">About BrandColors</a>
-            </li>
-          </ul>
-        </nav>
-        <a className="skill-share" href="">
-          <span>
-            Brand<strong>Colors</strong> + DesignBombs
-          </span>{" "}
-          <p>
-            Learn how to make a website - we have put together an in-depth guide that will help you build your first
-            website with WordPress.
-          </p>
-        </a>
+        <div className="second-section">
+          <h2>The biggest collection of official brand color codes around. Curated by @brandcolors and friends.</h2>
+          <nav>
+            <ul>
+              <li>
+                <a href="">Suggest a Brand</a>
+              </li>
+              <li>
+                <a href="">About BrandColors</a>
+              </li>
+            </ul>
+          </nav>
+          <a className="skill-share" href="">
+            <span>
+              Brand<strong>Colors</strong> + DesignBombs
+            </span>{" "}
+            <p>
+              Learn how to make a website - we have put together an in-depth guide that will help you build your first
+              website with WordPress.
+            </p>
+          </a>
+        </div>
       </aside>
     </>
   );
